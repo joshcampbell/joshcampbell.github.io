389fbb37433634af8841284d95bb7c18fe2c7a86
Josh Campbell <campbelj@gmail.com>
2016-02-20 21:44:40 -0600

    Temporarily switch color scheme

--- render.sh
+++ render.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 set -e
 
-generate-md --layout thomasf-solarizedcssdark --input ./raw-markdown --output .
+#generate-md --layout thomasf-solarizedcssdark --input ./raw-markdown --output .
+generate-md --layout mixu-bootstrap-2col --input ./raw-markdown --output .
